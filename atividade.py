563823ff148ee25c9e842dbbe52f38b8be5004ce
brunoribeiro122 <93730878+brunoribeiro122@users.noreply.github.com>
2023-02-08 23:37:55 +0000

    atividade

---
+++ atividade.py
@@ -0,0 +1,8 @@
+h = (int(input("Entre com a altura: ")))
+B = (int(input("Entre com a base maior:")))
+b = (int(input("Entre com a base menor: ")))
+
+if (h == 0):
+    print ("Entre com um valor diferente para a altura")
+else:
+    print("O resultado é", h*(B+b)/2 )
