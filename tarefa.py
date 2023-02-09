c19afa95c4cf8f4275a06667f8e3cf9c7f216fdb
brunoribeiro122 <93730878+brunoribeiro122@users.noreply.github.com>
2023-02-09 00:04:41 +0000

    Tarefa 2 python

---
+++ tarefa.py
@@ -0,0 +1,6 @@
+nome = (input("Digite seu nome"))
+endereco = (input("Digite seu endereco"))
+cep = (input("Digite seu cep"))
+telefone = (input("Digite seu telefone"))
+
+print("Seu nome é: ",nome,"\nfdbseu endereco é: ", endereco, "\nSeu CEP é:", cep, "Seu telefone é: ", telefone) 
